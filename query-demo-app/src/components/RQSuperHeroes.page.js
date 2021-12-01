971b3a02bb0596aa1fc4d8409a5b55d6713444e8
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-12-01 18:57:42 +0545

    wrap app with QueryProvider and use of useQueryHook for data fetching

--- query-demo-app/src/components/RQSuperHeroes.page.js
+++ query-demo-app/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,20 @@
+import { useQuery } from "react-query";
+import { fetchSuperHeroes } from "../services/SuperHeroes";
 export const RQSuperHeroesPage = () => {
-    return <h2>React Query Super Heroes Page</h2>
+  
+  const { isLoading, data } = useQuery("super-heroes",fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
   }
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+    {
+        data?.data.map((hero)=>{
+            return <div key={hero.name}>{hero.name}</div>
+        })
+    }
+    </>
+  );
+};
